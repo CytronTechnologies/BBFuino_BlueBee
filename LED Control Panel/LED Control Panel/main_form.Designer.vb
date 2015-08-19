40fb17389475e30f229312d87daa944c9cbd7c12
syuhada210 <syuhada@cytron.com.my>
2015-08-19 14:17:06 +0800

    LED control panel

---
+++ LED Control Panel/LED Control Panel/main_form.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main_form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.comlbl = New System.Windows.Forms.Label()
+        Me.cbcom = New System.Windows.Forms.ComboBox()
+        Me.baudlbl = New System.Windows.Forms.Label()
+        Me.cbbaud = New System.Windows.Forms.ComboBox()
+        Me.btnsearch = New System.Windows.Forms.Button()
+        Me.btnconnect = New System.Windows.Forms.Button()
+        Me.btndisconnect = New System.Windows.Forms.Button()
+        Me.gb_transmit = New System.Windows.Forms.GroupBox()
+        Me.btnledblink = New System.Windows.Forms.Button()
+        Me.btnledoff = New System.Windows.Forms.Button()
+        Me.btnledon = New System.Windows.Forms.Button()
+        Me.txtstatus = New System.Windows.Forms.TextBox()
+        Me.lblstatus = New System.Windows.Forms.Label()
+        Me.txtcom = New System.Windows.Forms.TextBox()
+        Me.lblcom = New System.Windows.Forms.Label()
+        Me.gb_transmit.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'comlbl
+        '
+        Me.comlbl.AutoSize = True
+        Me.comlbl.Location = New System.Drawing.Point(27, 13)
+        Me.comlbl.Name = "comlbl"
+        Me.comlbl.Size = New System.Drawing.Size(31, 13)
+        Me.comlbl.TabIndex = 0
+        Me.comlbl.Text = "COM"
+        '
+        'cbcom
+        '
+        Me.cbcom.FormattingEnabled = True
+        Me.cbcom.Location = New System.Drawing.Point(83, 10)
+        Me.cbcom.Name = "cbcom"
+        Me.cbcom.Size = New System.Drawing.Size(121, 21)
+        Me.cbcom.TabIndex = 1
+        '
+        'baudlbl
+        '
+        Me.baudlbl.AutoSize = True
+        Me.baudlbl.Location = New System.Drawing.Point(27, 57)
+        Me.baudlbl.Name = "baudlbl"
+        Me.baudlbl.Size = New System.Drawing.Size(50, 13)
+        Me.baudlbl.TabIndex = 2
+        Me.baudlbl.Text = "Baudrate"
+        '
+        'cbbaud
+        '
+        Me.cbbaud.FormattingEnabled = True
+        Me.cbbaud.Location = New System.Drawing.Point(83, 54)
+        Me.cbbaud.Name = "cbbaud"
+        Me.cbbaud.Size = New System.Drawing.Size(121, 21)
+        Me.cbbaud.TabIndex = 3
+        '
+        'btnsearch
+        '
+        Me.btnsearch.Location = New System.Drawing.Point(287, 13)
+        Me.btnsearch.Name = "btnsearch"
+        Me.btnsearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnsearch.TabIndex = 4
+        Me.btnsearch.Text = "Search"
+        Me.btnsearch.UseVisualStyleBackColor = True
+        '
+        'btnconnect
+        '
+        Me.btnconnect.Location = New System.Drawing.Point(231, 52)
+        Me.btnconnect.Name = "btnconnect"
+        Me.btnconnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnconnect.TabIndex = 5
+        Me.btnconnect.Text = "Connect"
+        Me.btnconnect.UseVisualStyleBackColor = True
+        '
+        'btndisconnect
+        '
+        Me.btndisconnect.Location = New System.Drawing.Point(339, 52)
+        Me.btndisconnect.Name = "btndisconnect"
+        Me.btndisconnect.Size = New System.Drawing.Size(75, 23)
+        Me.btndisconnect.TabIndex = 6
+        Me.btndisconnect.Text = "Disconnect"
+        Me.btndisconnect.UseVisualStyleBackColor = True
+        '
+        'gb_transmit
+        '
+        Me.gb_transmit.Controls.Add(Me.btnledblink)
+        Me.gb_transmit.Controls.Add(Me.btnledoff)
+        Me.gb_transmit.Controls.Add(Me.btnledon)
+        Me.gb_transmit.Location = New System.Drawing.Point(83, 108)
+        Me.gb_transmit.Name = "gb_transmit"
+        Me.gb_transmit.Size = New System.Drawing.Size(95, 100)
+        Me.gb_transmit.TabIndex = 7
+        Me.gb_transmit.TabStop = False
+        Me.gb_transmit.Text = "Transmit"
+        '
+        'btnledblink
+        '
+        Me.btnledblink.Location = New System.Drawing.Point(7, 76)
+        Me.btnledblink.Name = "btnledblink"
+        Me.btnledblink.Size = New System.Drawing.Size(75, 23)
+        Me.btnledblink.TabIndex = 2
+        Me.btnledblink.Text = "LED blink"
+        Me.btnledblink.UseVisualStyleBackColor = True
+        '
+        'btnledoff
+        '
+        Me.btnledoff.Location = New System.Drawing.Point(6, 49)
+        Me.btnledoff.Name = "btnledoff"
+        Me.btnledoff.Size = New System.Drawing.Size(75, 23)
+        Me.btnledoff.TabIndex = 1
+        Me.btnledoff.Text = "LED off"
+        Me.btnledoff.UseVisualStyleBackColor = True
+        '
+        'btnledon
+        '
+        Me.btnledon.Location = New System.Drawing.Point(7, 20)
+        Me.btnledon.Name = "btnledon"
+        Me.btnledon.Size = New System.Drawing.Size(75, 23)
+        Me.btnledon.TabIndex = 0
+        Me.btnledon.Text = "LED on"
+        Me.btnledon.UseVisualStyleBackColor = True
+        '
+        'txtstatus
+        '
+        Me.txtstatus.Location = New System.Drawing.Point(231, 184)
+        Me.txtstatus.Name = "txtstatus"
+        Me.txtstatus.ReadOnly = True
+        Me.txtstatus.Size = New System.Drawing.Size(168, 20)
+        Me.txtstatus.TabIndex = 8
+        '
+        'lblstatus
+        '
+        Me.lblstatus.AutoSize = True
+        Me.lblstatus.Location = New System.Drawing.Point(228, 167)
+        Me.lblstatus.Name = "lblstatus"
+        Me.lblstatus.Size = New System.Drawing.Size(84, 13)
+        Me.lblstatus.TabIndex = 9
+        Me.lblstatus.Text = "Received status"
+        '
+        'txtcom
+        '
+        Me.txtcom.Location = New System.Drawing.Point(231, 130)
+        Me.txtcom.Name = "txtcom"
+        Me.txtcom.ReadOnly = True
+        Me.txtcom.Size = New System.Drawing.Size(168, 20)
+        Me.txtcom.TabIndex = 10
+        '
+        'lblcom
+        '
+        Me.lblcom.AutoSize = True
+        Me.lblcom.Location = New System.Drawing.Point(231, 108)
+        Me.lblcom.Name = "lblcom"
+        Me.lblcom.Size = New System.Drawing.Size(62, 13)
+        Me.lblcom.TabIndex = 11
+        Me.lblcom.Text = "COM status"
+        '
+        'main_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(447, 240)
+        Me.Controls.Add(Me.lblcom)
+        Me.Controls.Add(Me.txtcom)
+        Me.Controls.Add(Me.lblstatus)
+        Me.Controls.Add(Me.txtstatus)
+        Me.Controls.Add(Me.gb_transmit)
+        Me.Controls.Add(Me.btndisconnect)
+        Me.Controls.Add(Me.btnconnect)
+        Me.Controls.Add(Me.btnsearch)
+        Me.Controls.Add(Me.cbbaud)
+        Me.Controls.Add(Me.baudlbl)
+        Me.Controls.Add(Me.cbcom)
+        Me.Controls.Add(Me.comlbl)
+        Me.Name = "main_form"
+        Me.Text = "LED Control Panel"
+        Me.gb_transmit.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents comlbl As System.Windows.Forms.Label
+    Friend WithEvents cbcom As System.Windows.Forms.ComboBox
+    Friend WithEvents baudlbl As System.Windows.Forms.Label
+    Friend WithEvents cbbaud As System.Windows.Forms.ComboBox
+    Friend WithEvents btnsearch As System.Windows.Forms.Button
+    Friend WithEvents btnconnect As System.Windows.Forms.Button
+    Friend WithEvents btndisconnect As System.Windows.Forms.Button
+    Friend WithEvents gb_transmit As System.Windows.Forms.GroupBox
+    Friend WithEvents btnledblink As System.Windows.Forms.Button
+    Friend WithEvents btnledoff As System.Windows.Forms.Button
+    Friend WithEvents btnledon As System.Windows.Forms.Button
+    Friend WithEvents txtstatus As System.Windows.Forms.TextBox
+    Friend WithEvents lblstatus As System.Windows.Forms.Label
+    Friend WithEvents txtcom As System.Windows.Forms.TextBox
+    Friend WithEvents lblcom As System.Windows.Forms.Label
+
+End Class
---
+++ LED Control Panel/LED Control Panel/main_form.vb
@@ -0,0 +1,158 @@
+﻿Imports System
+Imports System.IO.Ports
+Imports System.Threading
+Public Class main_form
+    Dim COMport As New SerialPort
+    Private Sub main_form_Load(ByVal sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        cbbaud.Items.Add(9600)
+        cbbaud.Items.Add(19200)
+        cbbaud.Items.Add(38400)
+        cbbaud.Items.Add(115200)
+        cbbaud.Text = cbbaud.Items.Item(0)
+
+        For i As Integer = 0 To My.Computer.Ports.SerialPortNames.Count - 1
+            cbcom.Items.Add(My.Computer.Ports.SerialPortNames(i))
+        Next
+        cbcom.Text = cbcom.Items.Item(0)
+        btndisconnect.Enabled = False
+        btnledon.Enabled = False
+        btnledoff.Enabled = False
+        btnledblink.Enabled = False
+    End Sub
+
+    Private Sub btnsearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnsearch.Click
+        cbcom.Items.Clear()
+        For i As Integer = 0 To My.Computer.Ports.SerialPortNames.Count - 1
+            cbcom.Items.Add(My.Computer.Ports.SerialPortNames(i))
+        Next
+    End Sub
+
+    Private Sub btnconnect_Click_1(sender As System.Object, e As System.EventArgs) Handles btnconnect.Click
+        btnconnect.Enabled = False
+        btnsearch.Enabled = False
+        COMport.PortName = cbcom.Text
+        COMport.BaudRate = cbbaud.Text
+        COMport.DataBits = 8
+        COMport.Parity = Parity.None
+        COMport.StopBits = StopBits.One
+        COMport.WriteTimeout = 500
+        COMport.ReadTimeout = 500
+        Try
+            COMport.Open()
+            btnledon.Enabled = True
+            btnledoff.Enabled = True
+            btnledblink.Enabled = True
+            btnconnect.Enabled = False
+            btndisconnect.Enabled = True
+            txtcom.Text = COMport.PortName & " is connected"
+        Catch ex As Exception
+            COMport.Close()
+            MsgBox("The choosen COM port is not available")
+            txtcom.Text = "Please choose other COM ports"
+            btnsearch.Enabled = True
+            btnconnect.Enabled = True
+            btndisconnect.Enabled = False
+            btnledon.Enabled = False
+            btnledoff.Enabled = False
+            btnledblink.Enabled = False
+        End Try
+    End Sub
+
+    Private Sub btndisconnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndisconnect.Click
+        COMport.Close()
+        btnsearch.Enabled = True
+        btndisconnect.Enabled = False
+        btnconnect.Enabled = True
+        btnledon.Enabled = False
+        btnledoff.Enabled = False
+        btnledblink.Enabled = False
+        txtstatus.Clear()
+        txtcom.Text = COMport.PortName & " is disconnected"
+    End Sub
+
+    Private Sub btnledon_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnledon.Click
+        If (COMport.IsOpen) Then
+            btnledon.Enabled = False
+            btnledoff.Enabled = True
+            btnledblink.Enabled = True
+            Dim send As Byte = 1
+            Dim rec_data As String
+            Try
+                COMport.Write(send)
+            Catch ex As Exception
+                MsgBox("Cannot send!")
+            End Try
+
+            Try
+                rec_data = COMport.ReadLine()
+                txtstatus.Text = rec_data
+            Catch ex As Exception
+                MsgBox("Received Failed")
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnledoff_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnledoff.Click
+        If (COMport.IsOpen) Then
+            btnledon.Enabled = True
+            btnledoff.Enabled = False
+            btnledblink.Enabled = True
+            Dim send As Byte = 2
+            Dim rec_data As String
+            Try
+                COMport.Write(send)
+            Catch ex As Exception
+                MsgBox("Cannot send!")
+            End Try
+
+            Try
+                rec_data = COMport.ReadLine()
+                txtstatus.Text = rec_data
+            Catch ex As Exception
+                MsgBox("Received Failed")
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnledblink_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnledblink.Click
+        If (COMport.IsOpen) Then
+            btnledon.Enabled = True
+            btnledoff.Enabled = True
+            btnledblink.Enabled = False
+            Dim send As Byte = 3
+            Dim rec_data As String
+
+            Try
+                COMport.Write(send)
+            Catch ex As Exception
+                MsgBox("Cannot send!")
+            End Try
+
+            Try
+                rec_data = COMport.ReadLine()
+                txtstatus.Text = rec_data
+            Catch ex As Exception
+                MsgBox("Received Failed")
+            End Try
+        End If
+    End Sub
+
+    Private Sub cbbaud_SelectedValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbbaud.SelectedValueChanged
+        If COMport.IsOpen = True Then
+            If String.Compare(cbbaud.Text, COMport.BaudRate) Then
+                MsgBox("Change COM while connected is not allowed")
+                cbbaud.Text = COMport.BaudRate
+            End If
+        End If
+    End Sub
+
+    Private Sub cbcom_SelectedValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbcom.SelectedValueChanged
+        If COMport.IsOpen = True Then
+            If String.Compare(cbcom.Text, COMport.PortName) Then
+                MsgBox("Change COM while connected is not allowed")
+                cbcom.Text = COMport.PortName
+            End If
+        End If
+    End Sub
+
+End Class
